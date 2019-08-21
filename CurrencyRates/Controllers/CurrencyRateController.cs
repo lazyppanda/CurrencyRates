33a3e39a4554c1194b9a92687a3e604dae1d37b8
Alexey Badenkov <lazyppanda@gmail.com>
2019-08-21 15:32:48 +0300

    Finished work with getting api from cbr

--- CurrencyRates/Controllers/CurrencyRateController.cs
+++ CurrencyRates/Controllers/CurrencyRateController.cs
@@ -4,22 +4,24 @@ using System.Linq;
 using System.Net;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
-
+using Newtonsoft.Json;
 
 namespace CurrencyRates.Controllers
 {
     public class CurrencyRateController : Controller
     {
-        String ApiLink = "https://www.cbr-xml-daily.ru/daily_json.js";
+        string ApiLink = "https://www.cbr-xml-daily.ru/daily_json.js";
         // GET: /<controller>/
         public IActionResult Index()
         {
             using (WebClient wc = new WebClient())
             {
                 var json = wc.DownloadString(ApiLink);
-                ViewData["Json"] = json;
+                JsonApi jsonApi = JsonConvert.DeserializeObject<JsonApi>(json);
+                ViewData["AUD"] = jsonApi.Valute["AUD"].ID;
+
             }
-            return View();
+                return View();
         }
     }
 }
---
+++ CurrencyRates/Controllers/JsonApi.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CurrencyRates.Controllers
+{
+    internal class JsonApi
+    {
+        public DateTime Date { get; set; }
+        public DateTime PreviousDate { get; set; }
+        public string PreviousURL { get; set; }
+        public DateTime Timestamp { get; set; }
+
+        public IDictionary<string, Rate> Valute { get; set; }
+    }
+
+    public class Rate
+    {
+        public string ID { get; set; }
+        public int NumCode { get; set; }
+        public int Nominal { get; set; }
+        public string Name { get; set; }
+        public double Value { get; set; }
+        public double Previous { get; set; }
+    }
+}
