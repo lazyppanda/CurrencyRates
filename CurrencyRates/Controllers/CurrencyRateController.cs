7e379422d905e88f7565109915dea2edb33d6eba
Alexey Badenkov <lazyppanda@gmail.com>
2019-08-22 17:03:49 +0300

    Just another commit

--- CurrencyRates/Controllers/CurrencyRateController.cs
+++ CurrencyRates/Controllers/CurrencyRateController.cs
@@ -10,6 +10,41 @@ namespace CurrencyRates.Controllers
 {
     public class CurrencyRateController : Controller
     {
+        string[] valutes ={ "AUS",
+                            "AZN",
+                            "GBP",
+                            "AMD",
+                            "BYN",
+                            "BGN",
+                            "BRL",
+                            "HUF",
+                            "HKD",
+                            "DKK",
+                            "USD",
+                            "EUR",
+                            "INR",
+                            "KZT",
+                            "CAD",
+                            "KGS",
+                            "CNY",
+                            "MDL",
+                            "NOK",
+                            "RLN",
+                            "RON",
+                            "XDR",
+                            "SGD",
+                            "TJS",
+                            "TRY",
+                            "TMT",
+                            "UZS",
+                            "UAH",
+                            "CZK",
+                            "SEK",
+                            "CHF",
+                            "ZAR",
+                            "KRW",
+                            "JPY",
+        };
         string ApiLink = "https://www.cbr-xml-daily.ru/daily_json.js";
         // GET: /<controller>/
         public IActionResult Index()
@@ -17,9 +52,9 @@ namespace CurrencyRates.Controllers
             using (WebClient wc = new WebClient())
             {
                 var json = wc.DownloadString(ApiLink);
-                JsonApi jsonApi = JsonConvert.DeserializeObject<JsonApi>(json);
-                ViewData["AUD"] = jsonApi.Valute["AUD"].ID;
-
+                JsonApi Rates = JsonConvert.DeserializeObject<JsonApi>(json);
+                ViewData["Valutes"] =  valutes;
+                ViewData["Rates"] = Rates;
             }
                 return View();
         }
